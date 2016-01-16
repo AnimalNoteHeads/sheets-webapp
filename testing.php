b9be6a4d3c4846951399f4f0e30e6536e8c9f1cf
Michal Ozaniak <ozaniak.michal@gmail.com>
2016-01-16 15:10:29 +0100

    Initial files created

---
+++ testing.php
@@ -0,0 +1,11 @@
+Converter from lilypond code to pdf: <br /><br />
+
+<form method="post" action="lilypond_converter.php">
+    
+    <textarea name="lilypond_code">
+        
+    </textarea>
+    
+    <input type="submit" value="Convert" />
+    
+</form>
