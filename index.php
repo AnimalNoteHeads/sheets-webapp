e69321d57b91de11fe37383c54fce20f3909c876
Andreas Larsen <andreas@larsenwork.com>
2016-01-16 22:16:22 +0100

    more space

--- index.php
+++ index.php
@@ -2,26 +2,32 @@ Converter from lilypond code to pdf: <br /><br />
 
 <form method="post" action="lilypond_converter.php">
 
-    Your name:<br>
+    Your name:<br />
     <input type="text" name="author">
 
-    Song language:<br>
+    <br /><br />
+
+    Song language:<br />
     <select name="language">
       <option value="en" selected>English</option>
       <option value="da">Danish</option>
     </select>
 
-    Song title:<br>
-    <input type="text" name="title">
+    <br /><br />
 
-    Song key:<br>
+    Song title:<br />
+    <input type="text" name="title"><br />
+
+    <br /><br />
+
+    Song key:<br />
     <select name="key">
       <option value="C" selected>C-major</option>
       <option value="Cm">C-minor</option>
       <option value="D">D-major</option>
       <option value="Dm">D-minor</option>
       <option value="E">E-major</option>
-      <option value="Em">E-minor</option>      
+      <option value="Em">E-minor</option>
       <option value="F">F-major</option>
       <option value="Fm">F-minor</option>
       <option value="G">G-major</option>
@@ -32,6 +38,8 @@ Converter from lilypond code to pdf: <br /><br />
       <option value="Bm">B-minor</option>
     </select>
 
+    <br /><br />
+
     <textarea name="lilypond_code" style="width: 800px; height: 400px;">c'8 c' c' d' e' e' e'4 |
   d'8 d' d' e' c'4 c' |
   e'4 e'8 f' g'4 g'8 g' |
@@ -41,7 +49,7 @@ Converter from lilypond code to pdf: <br /><br />
   c'8 c' c' d' e' e' e'4 |
   d'8 d' d' e' c'2 </textarea>
 
-    <br />
+    <br /><br />
 
     <input type="submit" value="Convert" />
 
