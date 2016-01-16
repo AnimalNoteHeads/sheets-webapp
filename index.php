2f5a5ba51bab65a9a3f5b599f86a0da5769a9efb
Andreas Larsen <andreas@larsenwork.com>
2016-01-16 22:17:26 +0100

    indent fix

--- index.php
+++ index.php
@@ -41,13 +41,13 @@ Converter from lilypond code to pdf: <br /><br />
     <br /><br />
 
     <textarea name="lilypond_code" style="width: 800px; height: 400px;">c'8 c' c' d' e' e' e'4 |
-  d'8 d' d' e' c'4 c' |
-  e'4 e'8 f' g'4 g'8 g' |
-  f'8 f' f' g' e'2 |
-  c''4 c'' b' b'8 b' |
-  a'8 a' a' a' g'2 |
-  c'8 c' c' d' e' e' e'4 |
-  d'8 d' d' e' c'2 </textarea>
+d'8 d' d' e' c'4 c' |
+e'4 e'8 f' g'4 g'8 g' |
+f'8 f' f' g' e'2 |
+c''4 c'' b' b'8 b' |
+a'8 a' a' a' g'2 |
+c'8 c' c' d' e' e' e'4 |
+d'8 d' d' e' c'2 </textarea>
 
     <br /><br />
 
