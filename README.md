2c01a63bea5a3757b5ac48690927ba3e6d27be1c
Andreas Larsen <andreas@larsenwork.com>
2016-01-16 21:44:07 +0100

    link to pdf

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # sheets-webapp
-PHP and angular based 
+PHP and angular based
 
 
 ###Connect to gcloud
 gcloud compute ssh  animalnoteheads --zone europe-west1-c
+
+sudo -i
--- lilypond_converter.php
+++ lilypond_converter.php
@@ -6,7 +6,7 @@
     const SERVER_PATH = "/var/www/html";
     const CACHE_SERVER_PATH = SERVER_PATH."/".CACHE_PATH;
     const LILYPOND_DATA_PATH = "/usr/share/lilypond/2.18.2/fonts/svg";
-    
+
     // Initial cache cleaner
     $files = scandir('folder/');
     foreach($files as $file) {
@@ -14,39 +14,39 @@
             unlink($file);
         }
     }
-    
+
     // Initializing lilypond heads
-//    function recurse_copy($src,$dst) { 
-//        $dir = opendir($src); 
-//        @mkdir($dst); 
-//        while(false !== ( $file = readdir($dir)) ) { 
-//            if (( $file != '.' ) && ( $file != '..' )) { 
-//                if ( is_dir($src . '/' . $file) ) { 
-//                    recurse_copy($src . '/' . $file,$dst . '/' . $file); 
-//                } 
-//                else { 
-//                    copy($src . '/' . $file,$dst . '/' . $file); 
-//                } 
-//            } 
-//        } 
-//        closedir($dir); 
-//    } 
+//    function recurse_copy($src,$dst) {
+//        $dir = opendir($src);
+//        @mkdir($dst);
+//        while(false !== ( $file = readdir($dir)) ) {
+//            if (( $file != '.' ) && ( $file != '..' )) {
+//                if ( is_dir($src . '/' . $file) ) {
+//                    recurse_copy($src . '/' . $file,$dst . '/' . $file);
+//                }
+//                else {
+//                    copy($src . '/' . $file,$dst . '/' . $file);
+//                }
+//            }
+//        }
+//        closedir($dir);
+//    }
 //    if(!file_exists(LILYPOND_DATA_PATH."/eps")) {
 //        recurse_copy("data/eps", LILYPOND_DATA_PATH."/eps");
 //    }
 
     // Check if the service has necessary data
     if(isset($_POST['lilypond_code'])) {
-        
+
         $lilypond_code = <<<ABC
 language = "da"
 songtitle = "Lille Peter Edderkop"
 arranger = "Andreas Larsen"
-music = { 
+music = {
   \key c \major
-                
+
 ABC;
-        
+
 $lilypond_code .= $_POST['lilypond_code'];
 
 $lilypond_code .= <<<ABC
@@ -55,27 +55,28 @@ $lilypond_code .= <<<ABC
 
 \include "/var/www/html/scripts/AnimalNoteHeads.ly"
 ABC;
-        
+
     } else {
         exit("Lilypond code was not found in the POST");
     }
-    
+
     // Creating new .ly file in the cache
     $ly_filename = md5($lilypond_code.microtime().rand(0, 999999999));
     $ly_filename_full = $ly_filename.".ly";
     $ly_path = CACHE_PATH."/".$ly_filename_full;
     $ly_path_full = CACHE_SERVER_PATH."/".$ly_filename_full;
     file_put_contents($ly_path, $lilypond_code);
-    
+
     // Creating new .pdf file in the cache
     $pdf_filename_full = $ly_filename.".pdf";
     $pdf_path = CACHE_PATH."/".$pdf_filename_full;
     $pdf_path_full = CACHE_SERVER_PATH."/".$pdf_filename_full;
-    
+    $pdf_link = "<a href'/".$pdf_path."'></a>";
+
     $conversion_command = "lilypond -o"
             ." ".CACHE_SERVER_PATH."/"
             ." ".$ly_path_full;
     shell_exec($conversion_command);
-    
-    echo $pdf_path;
+
+    echo $pdf_link;
 ?>
