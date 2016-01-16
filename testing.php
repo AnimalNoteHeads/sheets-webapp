acb41c2f87148ecb419d916fb5b9572f1d3e3208
Michal Ozaniak <ozaniak.michal@gmail.com>
2016-01-16 15:23:25 +0100

    no message

--- testing.php
+++ testing.php
@@ -2,9 +2,11 @@ Converter from lilypond code to pdf: <br /><br />
 
 <form method="post" action="lilypond_converter.php">
     
-    <textarea name="lilypond_code">
+    <textarea name="lilypond_code" style="width: 800px; height: 400px;">
         
     </textarea>
+    
+    <br />
     
     <input type="submit" value="Convert" />
     
