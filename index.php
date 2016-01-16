51a432690c275d3554b65e21d145e818cde801a0
Andreas Larsen <andreas@larsenwork.com>
2016-01-16 21:19:39 +0100

    testing -> index

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
-<?php
+Converter from lilypond code to pdf: <br /><br />
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-?>
-
+<form method="post" action="lilypond_converter.php">
+    
+    <textarea name="lilypond_code" style="width: 800px; height: 400px;">c'8 c' c' d' e' e' e'4 |
+  d'8 d' d' e' c'4 c' |
+  e'4 e'8 f' g'4 g'8 g' |
+  f'8 f' f' g' e'2 |
+  c''4 c'' b' b'8 b' |
+  a'8 a' a' a' g'2 |
+  c'8 c' c' d' e' e' e'4 |
+  d'8 d' d' e' c'2 </textarea>
+    
+    <br />
+    
+    <input type="submit" value="Convert" />
+    
+</form>
