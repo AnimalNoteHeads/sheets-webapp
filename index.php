b20ba2c113a7bd21476b18aa5cd6829551acc9df
Michal Ozaniak <ozaniak.michal@gmail.com>
2016-01-17 12:45:20 +0100

    no message

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
         <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
         <meta charset="utf-8" />
         <title>Animal sheets creator</title>
-        
+
         <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 
@@ -14,51 +14,57 @@
         <!-- Latest compiled JavaScript -->
         <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
     </head>
+    <style type="text/css">
+        body {
+            padding: 16px;
+            background-color: #eaeaea;
+        }
+    </style>
     <body>
 
-Converter from lilypond code to pdf: <br /><br />
+        <h1>Converter from lilypond code to pdf: </h1><br /><br />
 
-<form method="post" action="lilypond_converter.php">
+        <form method="post" action="lilypond_converter.php">
 
-    Your name:<br />
-    <input type="text" name="author">
+            <label for="author">Your name:</label><br />
+            <input type="text" name="author" id="author">
 
-    <br /><br />
+            <br /><br />
 
-    Song language:<br />
-    <select name="language">
-      <option value="en" selected>English</option>
-      <option value="da">Danish</option>
-    </select>
+            <label for="language">Song language:</label><br />
+            <select name="language" id="language">
+                <option value="en" selected>English</option>
+                <option value="da">Danish</option>
+            </select>
 
-    <br /><br />
+            <br /><br />
 
-    Song title:<br />
-    <input type="text" name="title"><br />
+            <label for="song_title">Song title:</label><br />
+            <input type="text" name="title" id="song_title"><br />
 
-    <br /><br />
+            <br />
 
-    Song key:<br />
-    <select name="key">
-      <option value="\key c \major" selected>C-major</option>
-      <option value="\key c \minor">C-minor</option>
-      <option value="\key d \major">D-major</option>
-      <option value="\key d \minor">D-minor</option>
-      <option value="\key e \major">E-major</option>
-      <option value="\key e \minor">E-minor</option>
-      <option value="\key f \major">F-major</option>
-      <option value="\key f \minor">F-minor</option>
-      <option value="\key g \major">G-major</option>
-      <option value="\key g \minor">G-minor</option>
-      <option value="\key a \major">A-major</option>
-      <option value="\key a \minor">A-minor</option>
-      <option value="\key b \major">B-major</option>
-      <option value="\key b \minor">B-minor</option>
-    </select>
+            <label for="song_key">Song key:</label><br />
+            <select name="key" id="song_key">
+                <option value="\key c \major" selected>C-major</option>
+                <option value="\key c \minor">C-minor</option>
+                <option value="\key d \major">D-major</option>
+                <option value="\key d \minor">D-minor</option>
+                <option value="\key e \major">E-major</option>
+                <option value="\key e \minor">E-minor</option>
+                <option value="\key f \major">F-major</option>
+                <option value="\key f \minor">F-minor</option>
+                <option value="\key g \major">G-major</option>
+                <option value="\key g \minor">G-minor</option>
+                <option value="\key a \major">A-major</option>
+                <option value="\key a \minor">A-minor</option>
+                <option value="\key b \major">B-major</option>
+                <option value="\key b \minor">B-minor</option>
+            </select>
 
-    <br /><br />
+            <br /><br />
 
-    <textarea name="lilypond_code" style="width: 800px; height: 400px;">c'8 c' c' d' e' e' e'4 |
+            <textarea name="lilypond_code" style="width: 800px; height: 400px;">c'8 c' c' d' e' e' e'4 |
 d'8 d' d' e' c'4 c' |
 e'4 e'8 f' g'4 g'8 g' |
 f'8 f' f' g' e'2 |
@@ -67,11 +73,11 @@ a'8 a' a' a' g'2 |
 c'8 c' c' d' e' e' e'4 |
 d'8 d' d' e' c'2 </textarea>
 
-    <br /><br />
+            <br /><br />
 
-    <input type="submit" value="Convert" />
+            <input type="submit" value="Convert" />
 
-</form>
+        </form>
 
     </body>
 </html>
